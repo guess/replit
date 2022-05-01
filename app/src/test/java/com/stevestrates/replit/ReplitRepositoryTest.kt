78655f537136dbae995a3e37d9b9f7cbb4ad6c44
Steve Strates <steve@stevestrates.com>
2022-05-01 18:05:33 -0400

    Add tests for repo

--- app/src/test/java/com/stevestrates/replit/ReplitRepositoryTest.kt
+++ app/src/test/java/com/stevestrates/replit/ReplitRepositoryTest.kt
@@ -1,12 +1,33 @@
 package com.stevestrates.replit
 
-import org.junit.Test
+import io.mockk.every
+import io.mockk.mockk
 
-import org.junit.Assert.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import rx.Single
+import rx.observers.TestSubscriber
 
 class ReplitRepositoryTest {
+    private val api = mockk<IReplitApi>()
+    private val repo = ReplitRepository(api)
+    private val subscriber = TestSubscriber<String>()
+    private val expectedValue = "test"
+
+    @BeforeEach
+    fun setup() {
+        every { api.putExec(any(), any()) } returns Single.just(ExecResult(expectedValue))
+        repo.execPython("foo").subscribe(subscriber)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `it completes with no errors`() {
+        subscriber.assertCompleted()
+        subscriber.assertNoErrors()
+    }
+
+    @Test
+    fun `it correctly maps the result`() {
+        subscriber.assertValue(expectedValue)
     }
 }
